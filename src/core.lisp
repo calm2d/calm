3838081248bf1fd36a45ff158dddebfc136d092b
Vito Van <vito@sdf.org>
2022-06-20 10:37:10 +0800

    remove unused `button` parameter in `on-mousemotion`

--- src/core.lisp
+++ src/core.lisp
@@ -44,7 +44,7 @@
 (defun on-keydown (key) (declare (ignore key)))
 (defun on-keyup (key) (declare (ignore key)))
 (defun on-mousewheel (x y direction) (declare (ignore x y direction)))
-(defun on-mousemotion (&key button x y) (declare (ignore x y)))
+(defun on-mousemotion (&key x y) (declare (ignore x y)))
 (defun on-mousebuttonup (&key button x y clicks) (declare (ignore button x y clicks)))
 (defun on-mousebuttondown (&key button x y clicks) (declare (ignore button x y clicks)))
 
